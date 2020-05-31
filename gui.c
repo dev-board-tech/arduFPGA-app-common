21b6f364e17aa558bdc2289a55537e44427a64b3
Iulian Gheorghiu <morgoth@devboard.tech>
2020-05-31 22:11:55 +0300

    Add new functionalities to the GUI explorer library.
    Add the option to limit the clip on up and down edge in case if we want to display statuses and other data that are displayed independently of the GUI explorer.
    Add the feature that the selected file will have displayed an asterisc.
    Add call's for left and right button's press.

--- gui.c
+++ gui.c
@@ -25,16 +25,20 @@
 #include "gui.h"
 #include "spi.h"
 #include "delay.h"
-#include "25flash.h"
+//#include "25flash.h"
 #include "fat_fs/inc/ff.h"
 #include "ffconf.h"
 #include "kbd.h"
 #include GUI_UTIL_FILE_INCLUDE
 
 
-
+uint8_t disp_up_limit = 0;
+uint8_t disp_dn_limit = 0;
 uint16_t menu_pos = 0;
 uint16_t menu_sel = 0;
+uint16_t file_checked = 0;
+uint16_t path_cnt = 0;
+uint16_t path_sel_cnt = 0;
 uint16_t items_scanned = 0;
 static const char* const str[] = {_VOLUME_STRS};
 FRESULT res = FR_OK;
@@ -45,14 +49,14 @@ FILINFO fInfo;
 FIL filObject;
 bool fs_mounted = false;
 
-extern _25flash_t flash_des;
-//extern _25flash_t flash_app;
-
 bool gui_initialized = false;
 
 const char file_ext_filter[][4] PROGMEM = GUI_LIST_OF_DISPLAYED_EXTENSIONS;
 
 void gui_init(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
+	disp_up_limit = 0;
+	disp_dn_limit = 7;
+	file_checked = -1;
 	kbd_init();
 	gui_idle(uSD, spi_screen, screen_buf);
 	gui_paint(uSD, spi_screen, screen_buf);
@@ -62,7 +66,9 @@ void gui_idle(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
 // Check if the FS is mounted, if it is, open the root directory.
 	if(uSD->fs_mounted != fs_mounted && uSD->fs_mounted) {
 		fs_mounted = true;
+#ifdef GUI_ACT_AT_uSD_INSERT
 		GUI_ACT_AT_uSD_INSERT(uSD, spi_screen, screen_buf);
+#endif
 	} else {
 		if(uSD->fs_mounted != fs_mounted && ~uSD->fs_mounted) {
 			fs_mounted = false;
@@ -83,11 +89,11 @@ void gui_idle(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
 		} else
 // Navigate the menu DOWN.
 		if(kbd_state & KBD_D_KEY) {
-			if((menu_sel < (menu_pos + 8)) && menu_sel < items_scanned - 1) {
+			if((menu_sel < (menu_pos + (disp_dn_limit + 1) - disp_up_limit)) && menu_sel < items_scanned - 1) {
 				menu_sel++;
 			}
-			if(menu_sel >= (menu_pos + 8) && res == FR_OK) {
-				menu_pos = (menu_sel - 7);
+			if(menu_sel >= (menu_pos + (disp_dn_limit + 1) - disp_up_limit) && res == FR_OK) {
+				menu_pos++;
 			}
 			gui_paint(uSD, spi_screen, screen_buf);
 		} else
@@ -99,11 +105,16 @@ void gui_idle(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
 					f_chdir(nameBuff);
 					menu_pos = 0;
 					menu_sel = 0;
+					path_cnt++;
 					gui_paint(uSD, spi_screen, screen_buf);
 				}
 			} else {
 //Open selected file and load it.
+				path_sel_cnt = path_cnt;
+#ifdef GUI_ACT_FUNC_ON_FILE_SELECT
 				GUI_ACT_FUNC_ON_FILE_SELECT(uSD, spi_screen, screen_buf);
+#endif
+				gui_paint(uSD, spi_screen, screen_buf);
 			}
 		} else
 		if(kbd_state & KBD_B_KEY) {
@@ -111,7 +122,18 @@ void gui_idle(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
 			f_chdir("..");
 			menu_pos = 0;
 			menu_sel = 0;
+			path_cnt--;
 			gui_paint(uSD, spi_screen, screen_buf);
+		} else
+		if(kbd_state & KBD_L_KEY) {
+#ifdef GUI_ACT_FUNC_ON_LEFT_BTN_PRESS
+			GUI_ACT_FUNC_ON_LEFT_BTN_PRESS(uSD, spi_screen, screen_buf);
+#endif
+		} else
+		if(kbd_state & KBD_R_KEY) {
+#ifdef GUI_ACT_FUNC_ON_RIGHT_BTN_PRESS
+			GUI_ACT_FUNC_ON_RIGHT_BTN_PRESS(uSD, spi_screen, screen_buf);
+#endif
 		}
 	}
 }
@@ -136,30 +158,34 @@ void gui_paint(mmc_sd_t *uSD, spi_t *spi_screen, uint8_t *screen_buf) {
 // Read each item in current directory.
 				res = f_readdir(&dirObject, &fInfo);
 // If there are more lines to display than the space on the display, exit the loop.
-				if(menu_scan == menu_pos + 8 || res != FR_OK) {
+				if(menu_scan == menu_pos + (disp_dn_limit + 1) - disp_up_limit || res != FR_OK) {
 					items_scanned = menu_scan + (res == FR_OK ? 1 : 0);
 					break;
 				}
 // Filter, display only files with .app extension and the sub directory's.
-				if(res == FR_OK && (/*!strcmp(fInfo.altname + strlen(fInfo.altname) - 3, "APP")*/ gui_is_extension(fInfo.altname + strlen(fInfo.altname) - 3) || fInfo.fattrib & AM_DIR)/*  && (fInfo.fattrib & AM_ARC)*/ ) {
+				if(res == FR_OK && (gui_is_extension(fInfo.altname + strlen(fInfo.altname) - 3) || fInfo.fattrib & AM_DIR)/*  && (fInfo.fattrib & AM_ARC)*/ ) {
 // Print only what is inside display window.
-					if(menu_scan >= menu_pos && menu_scan < menu_pos + 8) {
+					if(menu_scan >= menu_pos && menu_scan < menu_pos +  (disp_dn_limit + 1) - disp_up_limit) {
 						if(menu_scan == menu_sel) {
 							fattrib = fInfo.fattrib;
 							strcpy(nameBuff, fInfo.fname);
 						}
 // Check and print the selected line.
-						strcpy(tmpNameBuff, menu_scan == menu_sel ? ">":"  ");
+						if(file_checked == -1)
+							strcpy(tmpNameBuff, menu_scan == menu_sel ? ">":(menu_scan == file_checked && path_sel_cnt == path_cnt ? "*":"  "));
+						else {
+							strcpy(tmpNameBuff, menu_scan == menu_sel ? ">":"  ");
+						}
 // Concatenate the file/directory name.
 						strcat(tmpNameBuff, fInfo.fname);
-						ssd1306_put_rectangle(spi_screen, NULL, screen_buf, 0, (menu_scan - menu_pos) << 3, ssd1306_get_x(), 8, true, true);
-						ssd1306_draw_string(spi_screen, NULL, screen_buf, tmpNameBuff, 0, (menu_scan - menu_pos) << 3, false, false, 1, 0);
+						ssd1306_put_rectangle(spi_screen, NULL, screen_buf, 0, ((menu_scan - menu_pos) + disp_up_limit) << 3, ssd1306_get_x(), 8, true, true);
+						ssd1306_draw_string(spi_screen, NULL, screen_buf, tmpNameBuff, 0, ((menu_scan - menu_pos) + disp_up_limit) << 3, false, false, 1, 0);
 					}
-				menu_scan++;
+					menu_scan++;
 				}
 			}
 // If there are less that eight lines to print on the display, fill the rest of the display with the background color.
-			for (uint16_t tmp_cnt = (menu_scan - menu_pos); tmp_cnt < 8; tmp_cnt++) {
+			for (uint16_t tmp_cnt = ((menu_scan - menu_pos) + disp_up_limit); tmp_cnt < (disp_dn_limit + 1); tmp_cnt++) {
 				ssd1306_put_rectangle(spi_screen, NULL, screen_buf, 0, tmp_cnt << 3, ssd1306_get_x(), 8, true, true);
 			}
 		}
