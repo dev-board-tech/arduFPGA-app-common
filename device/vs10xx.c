ff1c85e7a152df2e88ef488ca10a26b1e4960881
Iulian Gheorghiu <morgoth@devboard.tech>
2020-09-13 19:09:13 +0300

    Some fixes initialization functions.

--- device/vs10xx.c
+++ device/vs10xx.c
@@ -34,18 +34,18 @@ static inline void vs10xx_deassert_cs() {
 	SPI_CS_6_PORT |= SPI_CS_6_PIN;
 }
 
-static inline void vs10xx_check_busy() {
-	while(~VS10xx_DREQ_DIR & VS10xx_DREQ_PIN);
+void vs10xx_check_busy() {
+	while(~VS10xx_DREQ_IN & VS10xx_DREQ_PIN);
 }
 
-static inline bool vs10xx_check_busy_skip() {
-	if(~VS10xx_DREQ_DIR & VS10xx_DREQ_PIN)
+bool vs10xx_check_busy_skip() {
+	if(~VS10xx_DREQ_IN & VS10xx_DREQ_PIN)
 		return true;
 	else
 		return false;
 }
 
-static void vs10xx_reg_write(spi_t *spi, uint8_t reg, uint16_t value) {
+void vs10xx_reg_write(spi_t *spi, uint8_t reg, uint16_t value) {
 	vs10xx_check_busy();
 	vs10xx_assert_cs();
 	unsigned char buffer[4];
@@ -55,42 +55,73 @@ static void vs10xx_reg_write(spi_t *spi, uint8_t reg, uint16_t value) {
 	buffer[3] = value;
 	spi_wr_buf(spi, buffer, 4);
 	vs10xx_deassert_cs();
-	delay_us(100);
 }
 
-static uint16_t vs10xx_reg_read(spi_t *spi, unsigned char reg) {
+uint16_t vs10xx_reg_read(spi_t *spi, unsigned char reg) {
 	uint16_t value;
 	vs10xx_check_busy();
 	vs10xx_assert_cs();
 	spi_wrd_byte(spi, VS_INS_READ);
 	spi_wrd_byte(spi, reg);
-	value = spi_wrd_byte(spi, 0xFF) <<8;
-	value += spi_wrd_byte(spi, 0xFF);
+	value = (spi_wrd_byte(spi, 0xFF) << 8) | spi_wrd_byte(spi, 0xFF);
 	vs10xx_deassert_cs();
-	delay_us(100);
 	return value;
 }
 
 void vs10xx_soft_reset(spi_t *spi) {
-	vs10xx_reg_write(spi, VS_SCI_MODE, 4);
-	vs10xx_reg_write(spi, VS_SCI_MODE, 0x0800);
-	vs10xx_reg_write(spi, VS_SCI_BASS, 0x7A00);
-	//vs10xx_reg_write(param, VS_SCI_CLOCKF, 0x2000);
+	*spi->spcr |= (1 << SPR1);
+	*spi->spcr &= ~(1 << SPR0);
+	*spi->spsr &= ~(1<<SPI2X);
 	vs10xx_check_busy();
+	vs10xx_reg_read(spi, VS_SCI_STATUS);
+	vs10xx_reg_write(spi, VS_SCI_MODE, VS_SM_SDINEW | VS_SM_RESET);
+	delay_ms(100);
+#ifdef VS10XX_CRISTAL_FREQ
+	vs10xx_set_pll(spi, VS10XX_CRISTAL_FREQ);
+#else
+	vs10xx_set_pll(spi, 12288000);
+#endif
+	delay_ms(10);
+	*spi->spcr &= ~(1 << SPR1);
+	*spi->spcr &= ~(1 << SPR0);
+	//*spi->spsr |= (1<<SPI2X);
+	//vs10xx_reg_write(param, VS_SCI_CLOCKF, 0x2000);
+    // Switch on the analog parts
+	//vs10xx_reg_write(spi, VS_SCI_AUDATA, 44101); // 44.1kHz stereo
+	// The next clocksetting allows SPI clocking at 5 MHz, 4 MHz is safe then.
+	//vs10xx_reg_write(spi, VS_SCI_CLOCKF, 6 << 12); // Normal clock settings multiplyer 3.0 = 12.2 MHz
+	delay_ms(1);
 }
 
 void vs10xx_hard_reset(spi_t *spi) {
+	*spi->spcr |= (1 << SPR1);
+	*spi->spcr &= ~(1 << SPR0);
+	*spi->spsr &= ~(1<<SPI2X);
 	vs10xx_assert_rst();
-	delay_ms(100);
+	delay_ms(1);
 	vs10xx_deassert_rst();
-	vs10xx_reg_write(spi, VS_SCI_MODE, 0x0800);
-	vs10xx_reg_write(spi, VS_SCI_BASS, 0x7A00);
+	delay_ms(200);
+#ifdef VS10XX_CRISTAL_FREQ
+	vs10xx_set_pll(spi, VS10XX_CRISTAL_FREQ);
+#else
+	vs10xx_set_pll(spi, 12288000);
+#endif
+	delay_ms(10);
+	*spi->spcr &= ~(1 << SPR1);
+	*spi->spcr &= ~(1 << SPR0);
+	//*spi->spsr |= (1<<SPI2X);
+	vs10xx_reg_read(spi, VS_SCI_STATUS);
+	vs10xx_reg_write(spi, VS_SCI_MODE, VS_SM_SDINEW);
 	//vs10xx_reg_write(param, VS_SCI_CLOCKF, 0x2000);
-	vs10xx_check_busy();
+    // Switch on the analog parts
+    //vs10xx_reg_write(spi, VS_SCI_AUDATA, 44101); // 44.1kHz stereo
+    // The next clocksetting allows SPI clocking at 5 MHz, 4 MHz is safe then.
+    //vs10xx_reg_write(spi, VS_SCI_CLOCKF, 6 << 12); // Normal clock settings multiplyer 3.0 = 12.2 MHz
+	delay_ms(1);
 }
 
 void vs10xx_set_pll(spi_t *spi, uint32_t qFreq) {
-	vs10xx_reg_write(spi, VS_SCI_CLOCKF, qFreq / 2000);
+	vs10xx_reg_write(spi, VS_SCI_CLOCKF, 0x8000 + (qFreq / 2000));
 }
 
 void vs10xx_set_volume(spi_t *spi, uint8_t r, uint8_t l) {
@@ -99,6 +130,10 @@ void vs10xx_set_volume(spi_t *spi, uint8_t r, uint8_t l) {
 
 uint16_t vs10xx_get_volume(spi_t *spi) {
 	return vs10xx_reg_read(spi, VS_SCI_VOL);
+}
+
+void vs10xx_set_bas(spi_t *spi, uint8_t bl, uint8_t bc, uint8_t tl, uint8_t tc ) {
+	vs10xx_reg_write(spi, VS_SCI_BASS, ((bl & 0x000F) << 12) | ((bc & 0x000F) << 8) | ((tl & 0x000F) << 4) | (tc & 0x000F));// 0x7AFA
 }
 
 void vs10xx_send_null(spi_t *spi, uint16_t len) {
@@ -128,12 +163,7 @@ bool vs10xx_send_1byte_data(spi_t *spi, uint8_t data) {
 }
 
 void vs10xx_init(spi_t *spi) {
+	VS10xx_RST_DIR |= VS10xx_RST_PIN;
+	VS10xx_DREQ_DIR &= ~VS10xx_DREQ_PIN;
 	vs10xx_hard_reset(spi);
-	vs10xx_soft_reset(spi);
-#ifdef VS10XX_CRISTAL_FREQ
-	vs10xx_set_pll(spi, VS10XX_CRISTAL_FREQ);
-#else
-	vs10xx_set_pll(spi, 12288000);
-#endif
-	delay_ms(100);
 }
--- device/vs10xx.h
+++ device/vs10xx.h
@@ -26,19 +26,492 @@
 #define VS_SCI_AIADDR		0x0A	//RW	Start address of application
 #define VS_SCI_VOL			0x0B	//RW	Volume control
 #define VS_SCI_AICTRL0		0x0C	//RW	Application control register 0
-#define VS_SCI_AICTR11		0x0D	//RW	Application control register 1
+#define VS_SCI_AICTRL1		0x0D	//RW	Application control register 1
 #define VS_SCI_AICTRL2		0x0E	//RW	Application control register 2
 #define VS_SCI_AICTRL3		0x0F	//RW	Application control register 3
+
+#define VS_SM_DIFF           (1<< 0)
+#define VS_SM_LAYER12        (1<< 1) /* VS1063, VS1053, VS1033, VS1011 */
+#define VS_SM_RECORD_PATH    (1<< 1) /* VS1103 */
+#define VS_SM_RESET          (1<< 2)
+#define VS_SM_CANCEL         (1<< 3) /* VS1063, VS1053 */
+#define VS_SM_OUTOFWAV       (1<< 3) /* VS1033, VS1003, VS1011 */
+#define VS_SM_OUTOFMIDI      (1<< 3) /* VS1103 */
+#define VS_SM_EARSPEAKER_LO  (1<< 4) /* VS1053, VS1033 */
+#define VS_SM_PDOWN          (1<< 4) /* VS1003, VS1103 */
+#define VS_SM_TESTS          (1<< 5)
+#define VS_SM_STREAM         (1<< 6) /* VS1053, VS1033, VS1003, VS1011 */
+#define VS_SM_ICONF          (1<< 6) /* VS1103 */
+#define VS_SM_EARSPEAKER_HI  (1<< 7) /* VS1053, VS1033 */
+#define VS_SM_DACT           (1<< 8)
+#define VS_SM_SDIORD         (1<< 9)
+#define VS_SM_SDISHARE       (1<<10)
+#define VS_SM_SDINEW         (1<<11)
+#define VS_SM_ENCODE         (1<<12) /* VS1063 */
+#define VS_SM_ADPCM          (1<<12) /* VS1053, VS1033, VS1003 */
+#define VS_SM_EARSPEAKER1103 (1<<12) /* VS1103 */
+#define VS_SM_ADPCM_HP       (1<<13) /* VS1033, VS1003 */
+#define VS_SM_LINE1          (1<<14) /* VS1063, VS1053 */
+#define VS_SM_LINE_IN        (1<<14) /* VS1033, VS1003, VS1103 */
+#define VS_SM_CLK_RANGE      (1<<15) /* VS1063, VS1053, VS1033 */
+#define VS_SM_ADPCM_1103     (1<<15) /* VS1103 */
+
+/* SCI_STATUS bits */
+
+#define SS_REFERENCE_SEL_B  0 /* VS1063, VS1053 */
+#define SS_AVOL_B           0 /* VS1033, VS1003, VS1103, VS1011 */
+#define SS_AD_CLOCK_B       1 /* VS1063, VS1053 */
+#define SS_APDOWN1_B        2
+#define SS_APDOWN2_B        3
+#define SS_VER_B            4
+#define SS_VCM_DISABLE_B   10 /* VS1063, VS1053 */
+#define SS_VCM_OVERLOAD_B  11 /* VS1063, VS1053 */
+#define SS_SWING_B         12 /* VS1063, VS1053 */
+#define SS_DO_NOT_JUMP_B   15 /* VS1063, VS1053 */
+
+#define SS_REFERENCE_SEL (1<< 0) /* VS1063, VS1053 */
+#define SS_AVOL          (1<< 0) /* VS1033, VS1003, VS1103, VS1011 */
+#define SS_AD_CLOCK      (1<< 1) /* VS1063, VS1053 */
+#define SS_APDOWN1       (1<< 2)
+#define SS_APDOWN2       (1<< 3)
+#define SS_VER           (1<< 4)
+#define SS_VCM_DISABLE   (1<<10) /* VS1063, VS1053 */
+#define SS_VCM_OVERLOAD  (1<<11) /* VS1063, VS1053 */
+#define SS_SWING         (1<<12) /* VS1063, VS1053 */
+#define SS_DO_NOT_JUMP   (1<<15) /* VS1063, VS1053 */
+
+#define SS_SWING_BITS     3
+#define SS_SWING_MASK     0x7000
+#define SS_VER_BITS       4
+#define SS_VER_MASK       0x00f0
+#define SS_AVOL_BITS      2
+#define SS_AVOL_MASK      0x0003
+
+#define SS_VER_VS1001 0x00
+#define SS_VER_VS1011 0x10
+#define SS_VER_VS1002 0x20
+#define SS_VER_VS1003 0x30
+#define SS_VER_VS1053 0x40
+#define SS_VER_VS8053 0x40
+#define SS_VER_VS1033 0x50
+#define SS_VER_VS1063 0x60
+#define SS_VER_VS1103 0x70
+
+
+/* SCI_BASS bits */
+
+#define ST_AMPLITUDE_B 12
+#define ST_FREQLIMIT_B  8
+#define SB_AMPLITUDE_B  4
+#define SB_FREQLIMIT_B  0
+
+#define ST_AMPLITUDE (1<<12)
+#define ST_FREQLIMIT (1<< 8)
+#define SB_AMPLITUDE (1<< 4)
+#define SB_FREQLIMIT (1<< 0)
+
+#define ST_AMPLITUDE_BITS 4
+#define ST_AMPLITUDE_MASK 0xf000
+#define ST_FREQLIMIT_BITS 4
+#define ST_FREQLIMIT_MASK 0x0f00
+#define SB_AMPLITUDE_BITS 4
+#define SB_AMPLITUDE_MASK 0x00f0
+#define SB_FREQLIMIT_BITS 4
+#define SB_FREQLIMIT_MASK 0x000f
+
+
+/* SCI_CLOCKF bits */
+
+#define SC_MULT_B 13 /* VS1063, VS1053, VS1033, VS1103, VS1003 */
+#define SC_ADD_B  11 /* VS1063, VS1053, VS1033, VS1003 */
+#define SC_FREQ_B  0 /* VS1063, VS1053, VS1033, VS1103, VS1003 */
+
+#define SC_MULT (1<<13) /* VS1063, VS1053, VS1033, VS1103, VS1003 */
+#define SC_ADD  (1<<11) /* VS1063, VS1053, VS1033, VS1003 */
+#define SC_FREQ (1<< 0) /* VS1063, VS1053, VS1033, VS1103, VS1003 */
+
+#define SC_MULT_BITS 3
+#define SC_MULT_MASK 0xe000
+#define SC_ADD_BITS 2
+#define SC_ADD_MASK  0x1800
+#define SC_FREQ_BITS 11
+#define SC_FREQ_MASK 0x07ff
+
+/* The following macro is for VS1063, VS1053, VS1033, VS1003, VS1103.
+   Divide hz by two when calling if SM_CLK_RANGE = 1 */
+#define HZ_TO_SC_FREQ(hz) (((hz)-8000000+2000)/4000)
+
+/* The following macro is for VS1011.
+   The macro will automatically set the clock doubler if XTALI < 16 MHz */
+#define HZ_TO_SCI_CLOCKF(hz) ((((hz)<16000000)?0x8000:0)+((hz)+1000)/2000)
+
+/* Following are for VS1003 and VS1033 */
+#define SC_MULT_03_10X 0x0000
+#define SC_MULT_03_15X 0x2000
+#define SC_MULT_03_20X 0x4000
+#define SC_MULT_03_25X 0x6000
+#define SC_MULT_03_30X 0x8000
+#define SC_MULT_03_35X 0xa000
+#define SC_MULT_03_40X 0xc000
+#define SC_MULT_03_45X 0xe000
+
+/* Following are for VS1053 and VS1063 */
+#define SC_MULT_53_10X 0x0000
+#define SC_MULT_53_20X 0x2000
+#define SC_MULT_53_25X 0x4000
+#define SC_MULT_53_30X 0x6000
+#define SC_MULT_53_35X 0x8000
+#define SC_MULT_53_40X 0xa000
+#define SC_MULT_53_45X 0xc000
+#define SC_MULT_53_50X 0xe000
+
+/* Following are for VS1003 and VS1033 */
+#define SC_ADD_03_00X 0x0000
+#define SC_ADD_03_05X 0x0800
+#define SC_ADD_03_10X 0x1000
+#define SC_ADD_03_15X 0x1800
+
+/* Following are for VS1053 and VS1063 */
+#define SC_ADD_53_00X 0x0000
+#define SC_ADD_53_10X 0x0800
+#define SC_ADD_53_15X 0x1000
+#define SC_ADD_53_20X 0x1800
+
+
+/* SCI_WRAMADDR bits */
+
+#define SCI_WRAM_X_START           0x0000
+#define SCI_WRAM_Y_START           0x4000
+#define SCI_WRAM_I_START           0x8000
+#define SCI_WRAM_IO_START          0xC000
+#define SCI_WRAM_PARAMETRIC_START  0xC0C0 /* VS1063 */
+#define SCI_WRAM_Y2_START          0xE000 /* VS1063 */
+
+#define SCI_WRAM_X_OFFSET  0x0000
+#define SCI_WRAM_Y_OFFSET  0x4000
+#define SCI_WRAM_I_OFFSET  0x8000
+#define SCI_WRAM_IO_OFFSET 0x0000 /* I/O addresses are @0xC000 -> no offset */
+#define SCI_WRAM_PARAMETRIC_OFFSET (0xC0C0-0x1E00) /* VS1063 */
+#define SCI_WRAM_Y2_OFFSET 0x0000                  /* VS1063 */
+
+
+/* SCI_VOL bits */
+
+#define SV_LEFT_B  8
+#define SV_RIGHT_B 0
+
+#define SV_LEFT  (1<<8)
+#define SV_RIGHT (1<<0)
+
+#define SV_LEFT_BITS  8
+#define SV_LEFT_MASK  0xFF00
+#define SV_RIGHT_BITS 8
+#define SV_RIGHT_MASK 0x00FF
+
+
+/* SCI_MIXERVOL bits for VS1103 */
+
+#define SMV_ACTIVE_B 15
+#define SMV_GAIN3_B  10
+#define SMV_GAIN2_B   5
+#define SMV_GAIN1_B   0
+
+#define SMV_ACTIVE (1<<15)
+#define SMV_GAIN3  (1<<10)
+#define SMV_GAIN2  (1<< 5)
+#define SMV_GAIN1  (1<< 0)
+
+#define SMV_GAIN3_BITS 5
+#define SMV_GAIN3_MASK 0x7c00
+#define SMV_GAIN2_BITS 5
+#define SMV_GAIN2_MASK 0x04e0
+#define SMV_GAIN1_BITS 5
+#define SMV_GAIN1_MASK 0x001f
+
+
+/* SCI_ADPCMRECCTL bits for VS1103 */
+
+#define SARC_DREQ512_B    8
+#define SARC_OUTODADPCM_B 7
+#define SARC_MANUALGAIN_B 6
+#define SARC_GAIN4_B      0
+
+#define SARC_DREQ512    (1<<8)
+#define SARC_OUTODADPCM (1<<7)
+#define SARC_MANUALGAIN (1<<6)
+#define SARC_GAIN4      (1<<0)
+
+#define SARC_GAIN4_BITS 6
+#define SARC_GAIN4_MASK 0x003f
+
+
+/* SCI_RECQUALITY bits for VS1063 */
+
+#define RQ_MODE_B                   14
+#define RQ_MULT_B                   12
+#define RQ_OGG_PAR_SERIAL_NUMBER_B  11
+#define RQ_OGG_LIMIT_FRAME_LENGTH_B 10
+#define RQ_MP3_NO_BIT_RESERVOIR_B   10
+#define RQ_BITRATE_BASE_B            0
+
+#define RQ_MODE                   (1<<14)
+#define RQ_MULT                   (1<<12)
+#define RQ_OGG_PAR_SERIAL_NUMBER  (1<<11)
+#define RQ_OGG_LIMIT_FRAME_LENGTH (1<<10)
+#define RQ_MP3_NO_BIT_RESERVOIR   (1<<10)
+#define RQ_BITRATE_BASE           (1<< 0)
+
+#define RQ_MODE_BITS 2
+#define RQ_MODE_MASK 0xc000
+#define RQ_MULT_BITS 2
+#define RQ_MULT_MASK 0x3000
+#define RQ_BITRATE_BASE_BITS 9
+#define RQ_BITRATE_BASE_MASK 0x01ff
+
+#define RQ_MODE_QUALITY 0x0000
+#define RQ_MODE_VBR     0x4000
+#define RQ_MODE_ABR     0x8000
+#define RQ_MODE_CBR     0xc000
+
+#define RQ_MULT_10      0x0000
+#define RQ_MULT_100     0x1000
+#define RQ_MULT_1000    0x2000
+#define RQ_MULT_10000   0x3000
+
+
+/* SCI_RECMODE bits for VS1063 */
+
+#define RM_63_CODEC_B    15
+#define RM_63_AEC_B      14
+#define RM_63_UART_TX_B  13
+#define RM_63_PAUSE_B    11
+#define RM_63_NO_RIFF_B  10
+#define RM_63_FORMAT_B    4
+#define RM_63_ADC_MODE_B  0 
+
+#define RM_63_CODEC    (1<<15)
+#define RM_63_AEC      (1<<14)
+#define RM_63_UART_TX  (1<<13)
+#define RM_63_PAUSE    (1<<11)
+#define RM_63_NO_RIFF  (1<<10)
+#define RM_63_FORMAT   (1<< 4)
+#define RM_63_ADC_MODE (1<< 0)
+
+#define RM_63_FORMAT_BITS       4
+#define RM_63_FORMAT_MASK  0x00f0
+#define RM_63_ADCMODE_BITS      3
+#define RM_63_ADCMODE_MASK 0x0007
+
+#define RM_63_FORMAT_IMA_ADPCM  0x0000
+#define RM_63_FORMAT_PCM        0x0010
+#define RM_63_FORMAT_G711_ULAW  0x0020
+#define RM_63_FORMAT_G711_ALAW  0x0030
+#define RM_63_FORMAT_G722_ADPCM 0x0040
+#define RM_63_FORMAT_OGG_VORBIS 0x0050
+#define RM_63_FORMAT_MP3        0x0060
+
+#define RM_63_ADC_MODE_JOINT_AGC_STEREO 0x0000
+#define RM_63_ADC_MODE_DUAL_AGC_STEREO  0x0001
+#define RM_63_ADC_MODE_LEFT             0x0002
+#define RM_63_ADC_MODE_RIGHT            0x0003
+#define RM_63_ADC_MODE_MONO             0x0004
+
+
+/* SCI_RECMODE bits for VS1053 */
+
+#define RM_53_FORMAT_B    2
+#define RM_53_ADC_MODE_B  0 
+
+#define RM_53_FORMAT   (1<< 2)
+#define RM_53_ADC_MODE (1<< 0)
+
+#define RM_53_ADCMODE_BITS      2
+#define RM_53_ADCMODE_MASK 0x0003
+
+#define RM_53_FORMAT_IMA_ADPCM  0x0000
+#define RM_53_FORMAT_PCM        0x0004
+
+#define RM_53_ADC_MODE_JOINT_AGC_STEREO 0x0000
+#define RM_53_ADC_MODE_DUAL_AGC_STEREO  0x0001
+#define RM_53_ADC_MODE_LEFT             0x0002
+#define RM_53_ADC_MODE_RIGHT            0x0003
+
+
+/* VS1063 definitions */
+
+/* VS1063 / VS1053 Parametric */
+#define PAR_CHIP_ID                  0x1e00 /* VS1063, VS1053, 32 bits */
+#define PAR_VERSION                  0x1e02 /* VS1063, VS1053 */
+#define PAR_CONFIG1                  0x1e03 /* VS1063, VS1053 */
+#define PAR_PLAY_SPEED               0x1e04 /* VS1063, VS1053 */
+#define PAR_BITRATE_PER_100          0x1e05 /* VS1063 */
+#define PAR_BYTERATE                 0x1e05 /* VS1053 */
+#define PAR_END_FILL_BYTE            0x1e06 /* VS1063, VS1053 */
+#define PAR_RATE_TUNE                0x1e07 /* VS1063,         32 bits */
+#define PAR_PLAY_MODE                0x1e09 /* VS1063 */
+#define PAR_SAMPLE_COUNTER           0x1e0a /* VS1063,         32 bits */
+#define PAR_VU_METER                 0x1e0c /* VS1063 */
+#define PAR_AD_MIXER_GAIN            0x1e0d /* VS1063 */
+#define PAR_AD_MIXER_CONFIG          0x1e0e /* VS1063 */
+#define PAR_PCM_MIXER_RATE           0x1e0f /* VS1063 */
+#define PAR_PCM_MIXER_FREE           0x1e10 /* VS1063 */
+#define PAR_PCM_MIXER_VOL            0x1e11 /* VS1063 */
+#define PAR_EQ5_DUMMY                0x1e12 /* VS1063 */
+#define PAR_EQ5_LEVEL1               0x1e13 /* VS1063 */
+#define PAR_EQ5_FREQ1                0x1e14 /* VS1063 */
+#define PAR_EQ5_LEVEL2               0x1e15 /* VS1063 */
+#define PAR_EQ5_FREQ2                0x1e16 /* VS1063 */
+#define PAR_JUMP_POINTS              0x1e16 /*         VS1053 */
+#define PAR_EQ5_LEVEL3               0x1e17 /* VS1063 */
+#define PAR_EQ5_FREQ3                0x1e18 /* VS1063 */
+#define PAR_EQ5_LEVEL4               0x1e19 /* VS1063 */
+#define PAR_EQ5_FREQ4                0x1e1a /* VS1063 */
+#define PAR_EQ5_LEVEL5               0x1e1b /* VS1063 */
+#define PAR_EQ5_UPDATED              0x1e1c /* VS1063 */
+#define PAR_SPEED_SHIFTER            0x1e1d /* VS1063 */
+#define PAR_EARSPEAKER_LEVEL         0x1e1e /* VS1063 */
+#define PAR_SDI_FREE                 0x1e1f /* VS1063 */
+#define PAR_AUDIO_FILL               0x1e20 /* VS1063 */
+#define PAR_RESERVED0                0x1e21 /* VS1063 */
+#define PAR_RESERVED1                0x1e22 /* VS1063 */
+#define PAR_RESERVED2                0x1e23 /* VS1063 */
+#define PAR_RESERVED3                0x1e24 /* VS1063 */
+#define PAR_LATEST_SOF               0x1e25 /* VS1063,         32 bits */
+#define PAR_LATEST_JUMP              0x1e26 /*         VS1053 */
+#define PAR_POSITION_MSEC            0x1e27 /* VS1063, VS1053, 32 bits */
+#define PAR_RESYNC                   0x1e29 /* VS1063, VS1053 */
+
+/* The following addresses are shared between modes. */
+/* Generic pointer */
+#define PAR_GENERIC                  0x1e2a /* VS1063, VS1053 */
+
+/* Encoder mode */
+#define PAR_ENC_TX_UART_DIV          0x1e2a /* VS1063 */
+#define PAR_ENC_TX_UART_BYTE_SPEED   0x1e2b /* VS1063 */
+#define PAR_ENC_TX_PAUSE_GPIO        0x1e2c /* VS1063 */
+#define PAR_ENC_AEC_ADAPT_MULTIPLIER 0x1e2d /* VS1063 */
+#define PAR_ENC_RESERVED             0x1e2e /* VS1063 */
+#define PAR_ENC_CHANNEL_MAX          0x1e3c /* VS1063 */
+#define PAR_ENC_SERIAL_NUMBER        0x1e3e /* VS1063 */
+
+/* Decoding WMA */
+#define PAR_WMA_CUR_PACKET_SIZE      0x1e2a /* VS1063, VS1053, 32 bits */
+#define PAR_WMA_PACKET_SIZE          0x1e2c /* VS1063, VS1053, 32 bits */
+
+/* Decoding AAC */
+#define PAR_AAC_SCE_FOUND_MASK       0x1e2a /* VS1063, VS1053 */
+#define PAR_AAC_CPE_FOUND_MASK       0x1e2b /* VS1063, VS1053 */
+#define PAR_AAC_LFE_FOUND_MASK       0x1e2c /* VS1063, VS1053 */
+#define PAR_AAC_PLAY_SELECT          0x1e2d /* VS1063, VS1053 */
+#define PAR_AAC_DYN_COMPRESS         0x1e2e /* VS1063, VS1053 */
+#define PAR_AAC_DYN_BOOST            0x1e2f /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS    0x1e30 /* VS1063, VS1053 */
+#define PAR_AAC_SBR_PS_FLAGS         0x1e31 /* VS1063 */
+
+
+/* Decoding MIDI (VS1053) */
+#define PAR_MIDI_BYTES_LEFT          0x1e2a /*         VS1053, 32 bits */
+
+/* Decoding Vorbis */
+#define PAR_VORBIS_GAIN 0x1e2a       0x1e30 /* VS1063, VS1053 */
+
+
+/* Bit definitions for parametric registers with bitfields */
+#define PAR_CONFIG1_DIS_WMA_B     15 /* VS1063 */
+#define PAR_CONFIG1_DIS_AAC_B     14 /* VS1063 */
+#define PAR_CONFIG1_DIS_MP3_B     13 /* VS1063 */
+#define PAR_CONFIG1_DIS_FLAC_B    12 /* VS1063 */
+#define PAR_CONFIG1_DIS_CRC_B      8 /* VS1063 */
+#define PAR_CONFIG1_AAC_PS_B       6 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_B      4 /* VS1063, VS1053 */
+#define PAR_CONFIG1_MIDI_REVERB_B  0 /*         VS1053 */
+
+#define PAR_CONFIG1_DIS_WMA     (1<<15) /* VS1063 */
+#define PAR_CONFIG1_DIS_AAC     (1<<14) /* VS1063 */
+#define PAR_CONFIG1_DIS_MP3     (1<<13) /* VS1063 */
+#define PAR_CONFIG1_DIS_FLAC    (1<<12) /* VS1063 */
+#define PAR_CONFIG1_DIS_CRC     (1<< 8) /* VS1063 */
+#define PAR_CONFIG1_AAC_PS      (1<< 6) /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR     (1<< 4) /* VS1063, VS1053 */
+#define PAR_CONFIG1_MIDI_REVERB (1<< 0) /*         VS1053 */
+
+#define PAR_CONFIG1_AAC_PS_BITS  2      /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_PS_MASK  0x00c0 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_BITS 2      /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_MASK 0x0030 /* VS1063, VS1053 */
+
+#define PAR_CONFIG1_AAC_SBR_ALWAYS_UPSAMPLE    0x0000 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_SELECTIVE_UPSAMPLE 0x0010 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_NEVER_UPSAMPLE     0x0020 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_SBR_DISABLE            0x0030 /* VS1063, VS1053 */
+
+#define PAR_CONFIG1_AAC_PS_NORMAL              0x0000 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_PS_DOWNSAMPLED         0x0040 /* VS1063, VS1053 */
+#define PAR_CONFIG1_AAC_PS_DISABLE             0x00c0 /* VS1063, VS1053 */
+
+#define PAR_PLAY_MODE_SPEED_SHIFTER_ENA_B 6 /* VS1063 */
+#define PAR_PLAY_MODE_EQ5_ENA_B           5 /* VS1063 */
+#define PAR_PLAY_MODE_PCM_MIXER_ENA_B     4 /* VS1063 */
+#define PAR_PLAY_MODE_AD_MIXER_ENA_B      3 /* VS1063 */
+#define PAR_PLAY_MODE_VU_METER_ENA_B      2 /* VS1063 */
+#define PAR_PLAY_MODE_PAUSE_ENA_B         1 /* VS1063 */
+#define PAR_PLAY_MODE_MONO_ENA_B          0 /* VS1063 */
+
+#define PAR_PLAY_MODE_SPEED_SHIFTER_ENA (1<<6) /* VS1063 */
+#define PAR_PLAY_MODE_EQ5_ENA           (1<<5) /* VS1063 */
+#define PAR_PLAY_MODE_PCM_MIXER_ENA     (1<<4) /* VS1063 */
+#define PAR_PLAY_MODE_AD_MIXER_ENA      (1<<3) /* VS1063 */
+#define PAR_PLAY_MODE_VU_METER_ENA      (1<<2) /* VS1063 */
+#define PAR_PLAY_MODE_PAUSE_ENA         (1<<1) /* VS1063 */
+#define PAR_PLAY_MODE_MONO_ENA          (1<<0) /* VS1063 */
+
+#define PAR_VU_METER_LEFT_BITS  8      /* VS1063 */
+#define PAR_VU_METER_LEFT_MASK  0xFF00 /* VS1063 */
+#define PAR_VU_METER_RIGHT_BITS 8      /* VS1063 */
+#define PAR_VU_METER_RIGHT_MASK 0x00FF /* VS1063 */
+
+#define PAR_AD_MIXER_CONFIG_MODE_B 2 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_B 2 /* VS1063 */
+
+#define PAR_AD_MIXER_CONFIG_MODE_BITS 2      /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_MODE_MASK 0x000c /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_BITS 2      /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_MASK 0x0003 /* VS1063 */
+
+#define PAR_AD_MIXER_CONFIG_RATE_192K 0x0000 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_96K  0x0001 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_48K  0x0002 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_RATE_24K  0x0003 /* VS1063 */
+
+#define PAR_AD_MIXER_CONFIG_MODE_STEREO 0x0000 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_MODE_MONO   0x0040 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_MODE_LEFT   0x0080 /* VS1063 */
+#define PAR_AD_MIXER_CONFIG_MODE_RIGHT  0x00c0 /* VS1063 */
+
+#define PAR_AAC_SBR_AND_PS_STATUS_SBR_PRESENT_B       0 /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_UPSAMPLING_ACTIVE_B 1 /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_PS_PRESENT_B        2 /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_PS_ACTIVE_B         3 /* VS1063, VS1053 */
+
+#define PAR_AAC_SBR_AND_PS_STATUS_SBR_PRESENT       (1<<0) /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_UPSAMPLING_ACTIVE (1<<1) /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_PS_PRESENT        (1<<2) /* VS1063, VS1053 */
+#define PAR_AAC_SBR_AND_PS_STATUS_PS_ACTIVE         (1<<3) /* VS1063, VS1053 */
+
 
 #define VS_INS_WRITE		0x02
 #define VS_INS_READ			0x03
 /*#####################################################*/
-
+void vs10xx_check_busy();
+bool vs10xx_check_busy_skip();
+void vs10xx_reg_write(spi_t *spi, uint8_t reg, uint16_t value);
+uint16_t vs10xx_reg_read(spi_t *spi, unsigned char reg);
 void vs10xx_soft_reset(spi_t *spi);
 void vs10xx_hard_reset(spi_t *spi);
 void vs10xx_set_pll(spi_t *spi, uint32_t qFreq);
 void vs10xx_set_volume(spi_t *spi, uint8_t r, uint8_t l);
 uint16_t vs10xx_get_volume(spi_t *spi);
+void vs10xx_set_bas(spi_t *spi, uint8_t bl, uint8_t bc, uint8_t tl, uint8_t tc);
 void vs10xx_send_null(spi_t *spi, uint16_t len);
 bool vs10xx_send_32B_data(spi_t *spi, uint8_t *buffer);
 bool vs10xx_send_1byte_data(spi_t *spi, uint8_t data);
